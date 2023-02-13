1bc83d143cd876bbf0bac2fdc96b2feb8e1d9d0f
Alec Gomez <alec_gomez1@baylor.edu>
2023-02-13 15:20:46 -0600

    quiz submisison

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -1,4 +1,4 @@
-'''
+"""
 Due the outstanding performance of the Customer Service Represetatives (CSRs) in the marketing department, management
 has decided to reward them with a 10% increase in their salary. To evaluate the impact on the budget, you have been
 asked to run a report on the employee file and display the results of BEFORE AND AFTER the salary increase.
@@ -6,36 +6,40 @@ asked to run a report on the employee file and display the results of BEFORE AND
 You will create a dictionary that has the full employee name as the key and ONLY their new salary as the value.
 
 Iternate through the dictionary to print out their name and thier new salary (as in image)
-'''
+"""
 
 import csv
 
-#open the file
+# open the file
+employee = open("employee_data.csv", "r")
+employee_file = csv.reader(employee, delimiter=",")
+outfile = open("employee_data.csv", "w")
 
+# create an empty dictionary
+emp_names = {}
 
+# use a loop to iterate through the csv file
+for emp in employee:
+    print(employee)
 
+    # check if the employee fits the search criteria
+criteria = "employee"
 
-#create an empty dictionary
-
-
-#use a loop to iterate through the csv file
-
-
-    #check if the employee fits the search criteria
-
-
-    
+if criteria in emp_names:
+    print("fits criteria")
+else:
+    print("does not fit search criteria")
 
 print()
-print('=========================================')
+print("=========================================")
 print()
 
-#iternate through the dictionary and print out the key and value as per printout
+# iternate through the dictionary and print out the key and value as per printout
+for emp in employee:
+    print(f"Manager Name: ['First Name']['Last Name']", "Current Salary: ['Salary']")
+    print("Manager Name: 
 
 
 
-          
-        
 
-        
-    
+outfile.close
